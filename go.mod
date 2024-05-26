2f290d8776dcec917854fb3c3ee4e75970742730
coderajay94 <official.ajaykumar94@gmail.com>
2024-05-27 00:17:52 +0530

    start with logging using middleware

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module gokitdemo
 
 go 1.19
 
-require github.com/go-kit/kit v0.13.0
-
 require (
-	github.com/go-kit/log v0.2.0 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-kit/kit v0.13.0
+	github.com/go-kit/log v0.2.0
 )
+
+require github.com/go-logfmt/logfmt v0.5.1 // indirect
--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,7 +1,34 @@
 package middleware
 
-import "github.com/go-kit/log"
+import (
+	service "gokitdemo/services"
+
+	"time"
+
+	"github.com/go-kit/log"
+)
 
 type LoggingMiddleware struct {
 	Logger log.Logger
+	Next   service.StringService
+}
+
+func (mw LoggingMiddleware) UpperCaseMW(s string) (output string, err error) {
+	defer func(begin time.Time) {
+		mw.Logger.Log("method", "UpperCase", "input", s, "output", output, "err", err, "took:", time.Since(begin))
+	}(time.Now())
+	output, err = mw.Next.MakeUpperCase(s)
+	return
+}
+
+func (mw LoggingMiddleware) CountMW(s string) (output int) {
+	defer func(begin time.Time) {
+		mw.Logger.Log("method", "Count", "input", s, "output", output, "took:", time.Since(begin))
+	}(time.Now())
+	output = mw.Next.MakeCount(s)
+	return
+}
+
+func test() string {
+	return "can be imported"
 }
